5e3535a4ebc8aaf2759a1b2561c10d01261743fa
Mohd Faiz Qureshi <99100324+faiz-qrsh@users.noreply.github.com>
2024-03-18 17:41:18 +0530

    Added projects descriptions

--- iPortfolio/assets/js/main.js
+++ iPortfolio/assets/js/main.js
@@ -1,16 +1,5 @@
-/**
-* Template Name: iPortfolio
-* Updated: Mar 10 2023 with Bootstrap v5.2.3
-* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
-
-  /**
-   * Easy selector helper function
-   */
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -20,9 +9,6 @@
     }
   }
 
-  /**
-   * Easy event listener function
-   */
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
     if (selectEl) {
@@ -34,16 +20,12 @@
     }
   }
 
-  /**
-   * Easy on scroll event listener 
-   */
+  
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
 
-  /**
-   * Navbar links active state on scroll
-   */
+  
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
@@ -61,9 +43,7 @@
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
 
-  /**
-   * Scrolls to an element with header offset
-   */
+  
   const scrollto = (el) => {
     let elementPos = select(el).offsetTop
     window.scrollTo({
@@ -72,9 +52,7 @@
     })
   }
 
-  /**
-   * Back to top button
-   */
+  
   let backtotop = select('.back-to-top')
   if (backtotop) {
     const toggleBacktotop = () => {
@@ -88,18 +66,14 @@
     onscroll(document, toggleBacktotop)
   }
 
-  /**
-   * Mobile nav toggle
-   */
+  
   on('click', '.mobile-nav-toggle', function(e) {
     select('body').classList.toggle('mobile-nav-active')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
 
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
+  
   on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
       e.preventDefault()
@@ -126,9 +100,7 @@
     }
   });
 
-  /**
-   * Hero type effect
-   */
+  
   const typed = select('.typed')
   if (typed) {
     let typed_strings = typed.getAttribute('data-typed-items')
@@ -142,9 +114,7 @@
     });
   }
 
-  /**
-   * Skills animation
-   */
+  
   let skilsContent = select('.skills-content');
   if (skilsContent) {
     new Waypoint({
@@ -159,9 +129,7 @@
     })
   }
 
-  /**
-   * Porfolio isotope and filter
-   */
+  
   window.addEventListener('load', () => {
     let portfolioContainer = select('.portfolio-container');
     if (portfolioContainer) {
@@ -260,3 +228,85 @@
   new PureCounter();
 
 })()
+
+
+function putDetails(){
+  var contentID;
+  s = location.search;
+  if(s != '') {
+    var split = s.split('=');
+    contentID = split[1];
+  }
+  let para1=document.getElementById('para1');
+  let para2=document.getElementById('para2');
+  let name=document.getElementById('name');
+  switch(contentID){
+    case '1':
+      para1.innerHTML="EliteDwell Sol revolutionizes the landscape of home services, offering an extensive array of solutions ranging from fundamental plumbing and electrical services to specialized tailoring and more. Our platform is meticulously crafted to address the diverse needs of contemporary homeowners, providing a comprehensive hub for all their household requirements. Central to EliteDwell Sol is an advanced chatbot interface facilitating seamless communication between users and service providers, ensuring swift assistance and query resolution. Moreover, our platform integrates email confirmations and streamlined workflows to elevate the user experience, ensuring transparency and reliability throughout the service delivery process.";
+      para2.innerHTML="Complementing its core functionalities, EliteDwell Sol features a dedicated career portal, empowering skilled professionals to showcase their expertise and connect with a broad clientele. Through this innovative avenue, service providers can expand their business horizons and seize new opportunities, fostering a dynamic ecosystem of home service excellence. With EliteDwell Sol, homeowners can rest assured knowing that their domestic needs are met with efficiency, professionalism, and unparalleled convenience.";
+      name.innerHTML="EliteDwell Solution";
+      break;
+    case '2':
+      para1.innerHTML="The Online Exam Portal website represents a cutting-edge solution tailored to meet the evolving needs of educational institutions and organizations. Offering a versatile platform for exam management, the portal facilitates seamless creation and administration of various exam formats, providing educators and administrators with unprecedented flexibility and control. From traditional multiple-choice assessments to complex essay-based exams, the portal accommodates diverse testing requirements, ensuring comprehensive coverage across academic disciplines.";
+      para2.innerHTML="Central to the Online Exam Portal's functionality is its intuitive user interface, designed to streamline the exam-taking experience for students and candidates. With user-friendly navigation and responsive design, the portal ensures optimal accessibility across devices, empowering users to engage with assessments anytime, anywhere. Additionally, robust security measures are implemented to safeguard the integrity of exams, including secure authentication protocols and encryption techniques to prevent unauthorized access and ensure data confidentiality. Moreover, the Online Exam Portal offers real-time result processing capabilities, enabling instant feedback and analysis for both administrators and test-takers. Through advanced reporting features and analytics tools, educators gain valuable insights into student performance trends, facilitating data-driven decision-making and targeted interventions to enhance learning outcomes. With its comprehensive suite of features and commitment to innovation, the Online Exam Portal stands as a catalyst for academic excellence, empowering institutions to adapt and thrive in today's digital learning landscape.";
+      name.innerHTML="Quizsterly";
+      break;
+    case '3':
+      para1.innerHTML="The Event Management Website is a sophisticated platform tailored to streamline and elevate the process of organizing and hosting events of all scales and types. Designed with versatility and efficiency in mind, the website offers a comprehensive suite of tools and features to facilitate seamless event planning, coordination, and execution. From corporate conferences and trade shows to weddings and social gatherings, the platform caters to the diverse needs of event organizers, providing them with the resources and support necessary to create memorable and successful experiences.";
+      para2.innerHTML="At the heart of the Event Management Website is its intuitive interface, which empowers users to effortlessly navigate through the event planning process. With customizable templates, event organizers can easily create and customize event pages, complete with event details, schedules, ticketing options, and interactive maps. The platform also integrates robust communication tools, allowing organizers to efficiently communicate with attendees, sponsors, and vendors, ensuring clear and timely information dissemination throughout the event lifecycle. Furthermore, the Event Management Website offers powerful analytics and reporting capabilities, enabling organizers to gain valuable insights into event performance metrics, attendee demographics, and engagement levels. Armed with this data-driven intelligence, organizers can make informed decisions to optimize future events and maximize return on investment. With its seamless user experience, advanced features, and commitment to excellence, the Event Management Website sets a new standard for event planning and execution, empowering organizers to create unforgettable experiences that leave a lasting impact.";
+      name.innerHTML="RegEase";
+      break;
+    case '4':
+      para1.innerHTML="The Grievance Android App developed for SRMS College offers a streamlined platform for students and faculty to address and resolve grievances effectively. Through this app, users can submit their complaints or concerns regarding various aspects of college life, including academics, infrastructure, or administrative issues. The app facilitates seamless communication between users and college authorities, ensuring timely response and resolution of grievances.";
+      para2.innerHTML="Equipped with features such as real-time notifications and status updates, the Grievance Android App keeps users informed about the progress of their complaints and fosters transparency in the grievance redressal process. Moreover, the app includes functionalities for categorizing grievances based on their nature and severity, enabling college administrators to prioritize and address them efficiently. With its user-friendly interface and robust functionality, the app serves as a valuable tool for enhancing communication and addressing concerns within the college community.";
+      name.innerHTML="SRMS Grievance App";
+      break;
+      case '5':
+        para1.innerHTML="The Audio Extractor Android application offers a user-friendly interface for extracting audio from video files with unparalleled ease. Utilizing the FFMPEG library, renowned for its versatility and efficiency, the app ensures seamless processing of various video formats. Whether it's extracting soundtracks from movies, music videos, or personal recordings, users can rely on the app's robust functionality to obtain high-quality audio files quickly and effortlessly";
+        para2.innerHTML="With the Audio Extractor app, users can enjoy the convenience of extracting audio on the go, directly from their Android devices. The app's intuitive design and streamlined workflow make it accessible to users of all levels, from casual enthusiasts to professional editors. By harnessing the power of the FFMPEG library, the app delivers reliable performance and consistent results, ensuring that users can extract audio from their video files with confidence and precision.";
+        name.innerHTML="Audio Extracter";
+        break;
+
+        case '6':
+      para1.innerHTML="The WhatsApp Clone Android application offers a robust messaging platform that mirrors the functionality and user experience of the popular WhatsApp messenger. With features such as real-time messaging, voice calls, group chats, and multimedia sharing, the app provides users with a seamless communication experience. Built with modern technologies and scalable architecture, the app ensures fast and reliable messaging, even in low-bandwidth environments, making it suitable for users worldwide.";
+      para2.innerHTML="Designed with a focus on user privacy and security, the WhatsApp Clone app incorporates end-to-end encryption for all messages and media shared between users. Additionally, the app includes features such as two-factor authentication and fingerprint authentication to safeguard user accounts from unauthorized access. With its intuitive interface and comprehensive feature set, the WhatsApp Clone app aims to provide users with a familiar and secure messaging platform that meets their communication needs.";
+      name.innerHTML="Chitti";
+      break;
+
+      case '7':
+      para1.innerHTML="The Android application Chatbot revolutionizes user interaction through intelligent conversational interfaces. Leveraging natural language processing (NLP) and machine learning algorithms, the app interprets user queries and responds with relevant and contextually accurate information. From answering frequently asked questions to providing personalized recommendations, the Chatbot app streamlines user interactions and enhances user engagement across various domains.";
+      para2.innerHTML="With its sleek and user-friendly interface, the Chatbot app offers a seamless conversational experience, allowing users to interact with the app effortlessly. Whether it's seeking assistance with customer support inquiries, booking appointments, or accessing information on-demand, the app serves as a virtual assistant that caters to users' needs in real-time. By harnessing the power of AI-driven conversational technology, the Chatbot app aims to redefine user engagement and empower businesses to deliver exceptional customer experiences.";
+      name.innerHTML="MyBot";
+      break;
+
+      case '9':
+      para1.innerHTML="The Android application Calculator is a versatile tool that offers users a convenient and efficient way to perform various mathematical calculations on their mobile devices. With a user-friendly interface and intuitive design, the app allows users to perform basic arithmetic operations such as addition, subtraction, multiplication, and division, as well as more complex functions like square roots, percentages, and exponentiation. Equipped with a responsive keypad and clear display, the Calculator app ensures accurate and reliable calculations, making it an essential utility for users in everyday scenarios.";
+      para2.innerHTML="";
+      name.innerHTML="Calculator App";
+      break;
+
+      case '8':
+      para1.innerHTML="The Notes Android application offers a convenient and efficient way for users to capture, organize, and manage their notes and reminders on their mobile devices. With a simple and intuitive user interface, the app allows users to create notes, jot down thoughts, and set reminders effortlessly. Users can categorize their notes into different folders or tags, making it easy to find and access information when needed. Additionally, the app supports rich text formatting, enabling users to customize their notes with bold, italics, bullet points, and more, to suit their preferences.";
+      para2.innerHTML="Beyond basic note-taking functionality, the Notes app also provides advanced features such as cloud synchronization, allowing users to access their notes across multiple devices seamlessly. With automatic backup and sync capabilities, users can rest assured that their notes are safe and up-to-date, even if they switch devices or encounter technical issues. Furthermore, the app offers integration with popular productivity tools such as Google Calendar and Microsoft Outlook, enabling users to create tasks and reminders directly from their notes. Whether for personal use, work, or education, the Notes app serves as a reliable companion for users to stay organized and productive in their daily lives.";
+      name.innerHTML="MyNotes";
+      break;
+
+      case '10':
+      para1.innerHTML="The Simon Game Android application offers a modern adaptation of the classic Simon electronic game, providing users with an engaging and entertaining experience on their mobile devices. With its colorful and intuitive interface, the app challenges players to test their memory and reflexes by repeating a series of random sequences of colors and sounds. As players progress through the levels, the sequences become increasingly complex, offering a dynamic and immersive gameplay experience.";
+      para2.innerHTML="Built with user-friendly controls and responsive gameplay mechanics, the Simon Game app offers hours of addictive fun for players of all ages. Whether played solo to improve memory and concentration skills or in multiplayer mode to compete with friends and family, the app provides a stimulating and rewarding gaming experience. With its nostalgic charm and modern twist, the Simon Game app is sure to captivate and entertain players seeking a challenging and enjoyable mobile gaming experience.";
+      name.innerHTML="Simon Game";
+      break;
+      
+      case '11':
+      para1.innerHTML="The Network Security Implementation through Voice Biometric Android application is a cutting-edge solution designed to enhance network security by leveraging voice biometrics for user authentication. Through advanced voice recognition algorithms, the app verifies the identity of users based on their unique vocal characteristics, adding an extra layer of security to sensitive network systems and data.";
+      para2.innerHTML="Employing state-of-the-art encryption protocols and secure communication channels, the app ensures that user voice data remains confidential and protected from unauthorized access. By integrating voice biometrics into the network security framework, organizations can significantly reduce the risk of identity theft and unauthorized access to critical network resources, thereby bolstering the overall security posture of their IT infrastructure. With its innovative approach to network security, the Network Security Implementation through Voice Biometric app provides a robust and reliable solution for safeguarding sensitive information and maintaining the integrity of network systems.";
+      name.innerHTML="Voice Biometric";
+      break;
+
+      case '12':
+      para1.innerHTML="The Banking App for Android offers a simplified yet comprehensive solution for managing banking transactions on-the-go. With features like account balance checking, fund transfers, transaction history tracking, and bill payments, the app provides users with easy access to essential banking services from their mobile devices. Built with user convenience in mind, the app offers a user-friendly interface and intuitive navigation, making it suitable for users of all levels of technical proficiency.";
+      para2.innerHTML="In addition to basic banking functionalities, the app prioritizes security by implementing robust authentication mechanisms, such as biometric authentication and multi-factor authentication. Furthermore, the app ensures the security of users' financial data through encryption protocols and adherence to industry-standard security practices. With its seamless combination of convenience and security, the Banking App aims to enhance the banking experience for users, empowering them to manage their finances efficiently and securely from anywhere, at any time.";
+      name.innerHTML="Banking App";
+      break;
+  }
+}
